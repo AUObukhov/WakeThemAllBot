66fa335c40c48b936fe1491120f5edecd164a056
Artem Obukhov <a.obukhov@ecoislamicbank.kg>
2025-07-04 15:10:29 +0600

    initial commit

---
+++ src/main/java/kg/obukhov/wakethemallbot/WakeThemAllBotApplication.java
@@ -0,0 +1,13 @@
+package kg.obukhov.wakethemallbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WakeThemAllBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WakeThemAllBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kg/obukhov/wakethemallbot/WakeThemAllBotApplicationTests.java
@@ -0,0 +1,14 @@
+package kg.obukhov.wakethemallbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WakeThemAllBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+        // empty
+    }
+
+}
