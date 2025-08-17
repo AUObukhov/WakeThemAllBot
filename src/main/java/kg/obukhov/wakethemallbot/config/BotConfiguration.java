93f808ad38d31b8928d5c4eae34ae6e57abef631
Obukhov Artem <auobukhov@gmail.com>
2025-08-17 16:33:41 +0600

    Reapply "changed bot type back to LongPollingBot"
    This reverts commit 7ee40199e4d0b43e91b93fb46ddcd479d4eed603.

--- src/main/java/kg/obukhov/wakethemallbot/config/BotConfiguration.java
+++ src/main/java/kg/obukhov/wakethemallbot/config/BotConfiguration.java
@@ -5,15 +5,12 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import kg.obukhov.wakethemallbot.bot.Bot;
 import kg.obukhov.wakethemallbot.service.ChatUserService;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
-import org.telegram.telegrambots.meta.api.methods.updates.SetWebhook;
-import org.telegram.telegrambots.meta.api.objects.WebhookInfo;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
@@ -24,9 +21,6 @@ import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 @EntityScan("kg.obukhov.wakethemallbot.model")
 public class BotConfiguration {
 
-    @Value("${app.webhook-url}")
-    public String webhookUrl;
-
     @Bean
     public ObjectMapper objectMapper() {
         return new ObjectMapper()
@@ -35,24 +29,14 @@ public class BotConfiguration {
 
     @Bean
     public Bot bot(ChatUserService chatUserService, BotProperties botProperties) throws TelegramApiException {
-        Bot bot = new Bot(chatUserService, botProperties);
-        SetWebhook setWebhook = SetWebhook.builder()
-                .dropPendingUpdates(true)
-                .url(webhookUrl)
-                .build();
-
-        bot.setWebhook(setWebhook);
-
         TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
-        botsApi.registerBot(bot, setWebhook);
+
+        Bot bot = new Bot(chatUserService, botProperties);
+        botsApi.registerBot(bot);
+
         bot.registerCommands();
 
-        WebhookInfo webhookInfo = bot.getWebhookInfo();
-        if (webhookInfo == null) {
-            log.warn("WebhookInfo is null");
-        } else {
-            log.info("{} started! Webhook URL: {}", botProperties.getUsername(), webhookInfo.getUrl());
-        }
+        log.info("{} started!", botProperties.getUsername());
 
         return bot;
     }
